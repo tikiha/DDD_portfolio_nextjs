234fe756730bf136489cc59d4eb23e2633d8b56a
tiki <yskhj0316@gmail.com>
2023-06-13 14:00:37 +0900

    svg

---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const loading = () => {
+  return <div className="text-white w-full">loading</div>;
+};
+
+export default loading;
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,17 @@
-import { supabase } from "./../lib/supabaseClient";
-import { microcms } from "./../lib/microcmsClient";
 import classes from "../styles/home.module.scss";
 import Link from "next/link";
 
 async function Page() {
-  let { data: countries } = await supabase.from("countries").select();
-  const blog_datas = await microcms.get({ endpoint: "blogs" });
   return (
-    <ul>
-      {countries.map((country) => (
-        <li className={classes.countries} key={country.id}>
-          {country.name}
-        </li>
-      ))}
-      {blog_datas.contents.map((blog_data) => (
-        <Link href={`blog/${blog_data.id}`} key={blog_data.id}>
-          {blog_data.title}
-        </Link>
-      ))}
-    </ul>
+    <div className="w-screen flex flex-col">
+      <Link href={"/blog"} className="mt-10 bg-slate-500 p-2 rounded-lg w-fit">
+        microcms blog test→
+      </Link>
+
+      <Link href={"/svg"} className="mt-10 bg-slate-500 p-2 rounded-lg w-fit">
+        svg カスタマイズ→
+      </Link>
+    </div>
   );
 }
 
