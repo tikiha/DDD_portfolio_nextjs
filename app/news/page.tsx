f43b2fc1126b502bfb1a3bfa82c434d7360da212
tiki <yskhj0316@gmail.com>
2023-07-02 16:47:22 +0900

    max delete

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -28,7 +28,7 @@ async function Page() {
           border-b"
         >
           <div className="w-full flex items-center text-body">
-            <Calendar size={16} className="stroke-Light max-md:w-3" />
+            <Calendar size={16} className="stroke-Light w-3" />
             <span>&nbsp;{FormatDate(article.publishedAt)}</span>
           </div>
           <div className="h-full w-full flex flex-col items-center justify-center py-10">
@@ -53,7 +53,7 @@ async function Page() {
                   className="hover:scale-110 duration-150 object-cover object-center"
                 />
               ) : (
-                <div className="w-full h-full bg-slate-300 flex items-center justify-center transition-all hover:scale-110 max-lg:text-3xl max-md:text-h4">
+                <div className="w-full h-full bg-slate-300 flex items-center justify-center transition-all hover:scale-110 text-h4">
                   No Image
                 </div>
               )}
