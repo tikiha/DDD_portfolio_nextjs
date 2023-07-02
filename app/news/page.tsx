6bee1cfe2ec815d3710f7f425b15833ab0d1f2f4
tiki <yskhj0316@gmail.com>
2023-07-02 16:36:19 +0900

    lg max-lg

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -12,26 +12,26 @@ async function Page() {
   const articles = await microcms.get({ endpoint: "blogs" });
 
   return (
-    <div className="text-Light w-full h-full">
+    <RowSlide className="text-Light">
       <div className="bg-gradient-original w-full h-full fixed -z-10" />
       <div className="w-full h-16 bg-light fixed -translate-y-[100%] z-10" />
       <div
-        className="flex-shrink-0 flex items-center justify-center border-slate-400
-      w-full h-[33vh] border-b"
+        className="flex-shrink-0 lg:w-1/3 lg:h-full flex items-center justify-center lg:border-r border-slate-400
+      max-lg:border-b"
       >
         <h1 className="font-mont font-bold text-h1 ">NEWS</h1>
       </div>
       {articles.contents.map((article) => (
         <div
           key={article.id}
-          className="flex-shrink-0 border-slate-400 flex flex-col items-center justify-between 
-          w-full h-fit border-b p-4"
+          className="flex-shrink-0 lg:w-1/4 lg:h-full lg:border-r border-slate-400 lg:px-6 lg:py-10 flex flex-col items-center justify-between 
+          max-lg:border-b"
         >
           <div className="w-full flex items-center text-body">
-            <Calendar size={16} className="stroke-Light w-3" />
+            <Calendar size={16} className="stroke-Light max-md:w-3" />
             <span>&nbsp;{FormatDate(article.publishedAt)}</span>
           </div>
-          <div className="h-full w-full flex flex-col items-center justify-center py-10">
+          <div className="h-full w-full flex flex-col items-center justify-center max-lg:py-10">
             <Link
               href={`news/${article.id}`}
               className="text-h5 font-semibold line-clamp-2 h-14 hover:text-primary duration-300"
@@ -53,7 +53,7 @@ async function Page() {
                   className="hover:scale-110 duration-150 object-cover object-center"
                 />
               ) : (
-                <div className="w-full h-full bg-slate-300 flex items-center justify-center transition-all hover:scale-110 text-h4">
+                <div className="w-full h-full bg-slate-300 flex items-center justify-center transition-all hover:scale-110 max-lg:text-3xl max-md:text-h4">
                   No Image
                 </div>
               )}
@@ -65,7 +65,7 @@ async function Page() {
         </div>
       ))}
       <Footer />
-    </div>
+    </RowSlide>
   );
 }
 
