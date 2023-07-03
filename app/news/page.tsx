0fedbda60e3b63a7b324368a65cb8396ca9d4416
tiki <yskhj0316@gmail.com>
2023-07-03 13:34:53 +0900

    scrollbar

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -13,24 +13,25 @@ async function Page() {
 
   return (
     <RowSlide className="text-Light">
-      <div className="bg-gradient-original w-full h-full fixed -z-10" />
+      <div className="bg-gradient-original w-full h-full fixed -z-10 max-lg:fixed" />
       <div className="w-full h-16 bg-light fixed -translate-y-[100%] z-10" />
       <div
-        className="flex items-center justify-center border-slate-400
-      w-full h-[33vh] border-b"
+        className="flex-shrink-0 w-1/3 h-full flex items-center justify-center border-r border-slate-400
+      max-lg:w-full max-lg:h-[33vh] max-lg:border-b"
       >
         <h1 className="font-mont font-bold text-h1 ">NEWS</h1>
       </div>
       {articles.contents.map((article) => (
         <div
           key={article.id}
-          className="border-slate-400 flex flex-col items-center justify-between"
+          className="flex-shrink-0 w-1/4 h-full border-r border-slate-400 px-6 py-10 flex flex-col items-center justify-between 
+          max-lg:w-full max-lg:h-fit max-lg:border-b max-lg:p-4"
         >
           <div className="w-full flex items-center text-body">
-            <Calendar size={16} className="stroke-Light w-3" />
+            <Calendar size={16} className="stroke-Light max-md:w-3" />
             <span>&nbsp;{FormatDate(article.publishedAt)}</span>
           </div>
-          <div className="h-full w-full flex flex-col items-center justify-center py-10">
+          <div className="h-full w-full flex flex-col items-center justify-center max-lg:py-10">
             <Link
               href={`news/${article.id}`}
               className="text-h5 font-semibold line-clamp-2 h-14 hover:text-primary duration-300"
@@ -52,7 +53,7 @@ async function Page() {
                   className="hover:scale-110 duration-150 object-cover object-center"
                 />
               ) : (
-                <div className="w-full h-full bg-slate-300 flex items-center justify-center transition-all hover:scale-110 text-h4">
+                <div className="w-full h-full bg-slate-300 flex items-center justify-center transition-all hover:scale-110 max-lg:text-3xl max-md:text-h4">
                   No Image
                 </div>
               )}
