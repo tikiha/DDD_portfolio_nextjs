2cf209d0aea584c1de80bbb8b1fd3c394f9a9316
tiki <yskhj0316@gmail.com>
2023-07-04 19:50:49 +0900

    swiper

--- app/news/[id]/(components)/NewsCarousel.tsx
+++ app/news/[id]/(components)/NewsCarousel.tsx
@@ -7,7 +7,7 @@ import { Autoplay, Mousewheel } from "swiper";
 import "swiper/css";
 import "swiper/css/mousewheel";
 import "swiper/css/autoplay";
-import { Calendar } from "lucide-react";
+import { Calendar, Divide } from "lucide-react";
 import Link from "next/link";
 import FormatDate from "@/lib/formatDate";
 
@@ -37,65 +37,72 @@ const NewsCarousel = ({ articles }) => {
       <h1 className="font-mont font-bold text-h3 flex items-center justify-center mb-10">
         Latest news
       </h1>
-      <Swiper
-        className="w-full h-fit"
-        spaceBetween={50}
-        slidesPerView={2}
-        breakpoints={{
-          1024: {
-            slidesPerView: 4,
-          },
-          768: {
-            slidesPerView: 3.2,
-          },
-        }}
-        centeredSlides={true}
-        mousewheel={true}
-        autoplay={{
-          delay: 4000,
-          disableOnInteraction: false,
-        }}
-        loop={true}
-        modules={[Mousewheel, Autoplay]}
-      >
-        {Articles.map((article, index) => {
-          return (
-            <SwiperSlide key={`image ${article.id} ${index}`} className="mb-10">
-              <div className="w-full flex items-center text-sm text-mute">
-                <Calendar size={14} className="stroke-mute" />
-                <span>&nbsp;{FormatDate(article.publishedAt)}</span>
-              </div>
-              <div className="h-full w-full flex flex-col items-center justify-between">
-                <Link
-                  href={`news/${article.id}`}
-                  className="w-full overflow-hidden aspect-[16/9] relative my-4"
-                >
-                  {article.eyecatch ? (
-                    <img
-                      srcSet={article.eyecatch.url}
-                      alt={"eyecatch"}
-                      decoding="async"
-                      data-nimg="fill"
-                      sizes="100vw"
-                      className="hover:scale-110 duration-150 object-cover object-center"
-                    />
-                  ) : (
-                    <div className="w-full h-full bg-slate-300 flex items-center justify-center">
-                      No Image
-                    </div>
-                  )}
-                </Link>
-                <Link
-                  href={`news/${article.id}`}
-                  className="text-h5 hover:text-primary duration-300 max-lg:text-body"
-                >
-                  {article.title}
-                </Link>
-              </div>
-            </SwiperSlide>
-          );
-        })}
-      </Swiper>
+      {articles.contents.length > 1 ? (
+        <Swiper
+          className="w-full h-fit"
+          spaceBetween={50}
+          slidesPerView={2}
+          breakpoints={{
+            1024: {
+              slidesPerView: 4,
+            },
+            768: {
+              slidesPerView: 3.2,
+            },
+          }}
+          centeredSlides={true}
+          mousewheel={true}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          modules={[Mousewheel, Autoplay]}
+        >
+          {Articles.map((article, index) => {
+            return (
+              <SwiperSlide
+                key={`image ${article.id} ${index}`}
+                className="mb-10"
+              >
+                <div className="w-full flex items-center text-sm text-mute">
+                  <Calendar size={14} className="stroke-mute" />
+                  <span>&nbsp;{FormatDate(article.publishedAt)}</span>
+                </div>
+                <div className="h-full w-full flex flex-col items-center justify-between">
+                  <Link
+                    href={`news/${article.id}`}
+                    className="w-full overflow-hidden aspect-[16/9] relative my-4"
+                  >
+                    {article.eyecatch ? (
+                      <img
+                        srcSet={article.eyecatch.url}
+                        alt={"eyecatch"}
+                        decoding="async"
+                        data-nimg="fill"
+                        sizes="100vw"
+                        className="hover:scale-110 duration-150 object-cover object-center"
+                      />
+                    ) : (
+                      <div className="w-full h-full bg-slate-300 flex items-center justify-center">
+                        No Image
+                      </div>
+                    )}
+                  </Link>
+                  <Link
+                    href={`news/${article.id}`}
+                    className="text-h5 hover:text-primary duration-300 max-lg:text-body"
+                  >
+                    {article.title}
+                  </Link>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      ) : (
+        <div className="w-full text-center">その他の投稿はありません</div>
+      )}
     </div>
   );
 };
